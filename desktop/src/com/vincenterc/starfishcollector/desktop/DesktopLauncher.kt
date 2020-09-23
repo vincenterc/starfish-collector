f99f42e710c486f25f29e265f90b7320fa3dd254
vincenterc <chung.menghsun@gmail.com>
2020-09-23 22:27:50 +0800

    Setup starfish collector

--- desktop/src/com/vincenterc/starfishcollector/desktop/DesktopLauncher.kt
+++ desktop/src/com/vincenterc/starfishcollector/desktop/DesktopLauncher.kt
@@ -7,7 +7,11 @@ import com.vincenterc.starfishcollector.StarfishGame
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration()
+        val config = LwjglApplicationConfiguration().apply {
+            title = "Starfish Collector"
+            width = 800
+            height = 600
+        }
         LwjglApplication(StarfishGame(), config)
     }
 }
