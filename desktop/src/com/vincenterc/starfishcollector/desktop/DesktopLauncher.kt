9e3269a78f2fa6b43636323a2fd10e320bbdba1e
vincenterc <chung.menghsun@gmail.com>
2020-09-23 22:18:55 +0800

    Init

---
+++ desktop/src/com/vincenterc/starfishcollector/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.vincenterc.starfishcollector.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.vincenterc.starfishcollector.StarfishGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(StarfishGame(), config)
+    }
+}
